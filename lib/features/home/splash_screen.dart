c3c32064f9c714a2c723ea38f0746942bdc4a372
Bashir Alam <bashircse.nubtk@gmail.comgit config --global user.name>
2026-02-08 22:12:00 +0600

    Fix import paths and disable default widget test

--- lib/features/home/splash_screen.dart
+++ lib/features/home/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import '../language/language_selection_screen.dart';
+import './language/language_selection_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
